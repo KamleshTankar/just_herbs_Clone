36cd52cb24bd8e66ebfda2ff475afd059b476754
Kamlesh Dilip Tankar <kamleshtankar96@gmail.com>
2025-11-18 21:27:13 +0530

    exploreinfo and testimonials component done

--- client/src/components/Explores/ExploresInfo.jsx
+++ client/src/components/Explores/ExploresInfo.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import Banner from '../../assets/banner/Strip_banner.webp'
+
 const ExploresInfo = () => {
   return (
-    <div className=" h-40 p-6 my-2 bg-pink-200">
-      <h2>Explores Info</h2>
+    <div className=" w-[90%] h-56 p-6 my-2 mx-auto relative rounded-lg bg-cover bg-center" style={{ backgroundImage: `url(${Banner})` }}>
+      <h2 className='text-center font-titles font-semibold text-yellow-700 text-4xl'>Explores Info</h2>
       <p>Content goes here</p>
     </div>
   )
