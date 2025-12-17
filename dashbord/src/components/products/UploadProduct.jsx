58a9081f25c028c361a15eca2d8e5875784a20e3
Kamlesh Dilip Tankar <kamleshtankar96@gmail.com>
2025-12-17 18:35:35 +0530

    Add product api update with image upload

--- dashbord/src/components/products/UploadProduct.jsx
+++ dashbord/src/components/products/UploadProduct.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react'
 
-import { FaCloudUploadAlt, FaRegStar } from "react-icons/fa";
+import { FaCloudUploadAlt } from "react-icons/fa";
 import Banner from '../Banner'
 
 const UploadProduct = () => {
-  const [ptitle, setPTitle] = useState('');
-  const [pcategory, setPCategory] = useState('');
-  const [pimage,setPImage] = useState('');
-  const [pdescription, setPDescription] = useState('');
-  const [pprice, setPPrice] = useState('');
-  const [pquantity, setPQuantity] = useState('');
-  // const [pid,setPId] = useState('');
-  // const [subimage, setSubImage] = useState('');
+  const [pTitle, setPTitle] = useState('');
+  const [pCategory, setPCategory] = useState('');
+  const [pSubCategory, SetPSubCategory] = useState('');
+  const [pPrice, setPPrice] = useState('');
+  const [pQuantity, setPQuantity] = useState('');
+  const [pDescription, setPDescription] = useState('');
+  const [pSize, setPSize] = useState('');
+  const [pTags, setPTags] = useState('');
+  const [pDimensions, setPDimensions] = useState('');
+  const [pWeight, setPWeight ] = useState('');
+  const [pImages,setPImages] = useState([]);
 
   const AddProduct = () => {
-    const newProduct ={ptitle,pcategory,pimage,pdescription,pprice,pquantity}
+    const newProduct = {
+      pTitle, pCategory, pSubCategory, pImages, pDescription,
+      pPrice, pQuantity, pSize, pTags, pDimensions, pWeight
+    }
+    
+    if(!pTitle || !pCategory || !pSubCategory || !pPrice || !pQuantity || !pDescription || pImages.length===0 || !pSize || !pTags || !pDimensions || !pWeight) {
+      alert("Please fill all the fields");
+      return;
+    }
     console.log(newProduct);
   }
 
@@ -23,115 +34,126 @@ const UploadProduct = () => {
       <Banner page={"Upload Product"} />
       <form onSubmit={AddProduct} className="py-4">
         <section className="w-[96%] h-auto mx-auto mt-2 p-2 bg-white rounded-md">
-          <h2 className="text-2xl font-medium">Basic Infomation</h2>
+          <h2 className="text-2xl font-medium">Basic Information</h2>
           <div className="flex flex-col my-2">
             <label htmlFor="ProductName" className="text-lg font-medium">
               Product Name
             </label>
-            <input
-              type="text"
-              name="productname"
-              id="ProductName"
-              onChange={(e)=>{setPTitle(e.target.value)}}
+            <input type="text" name="ProductName" id="ProductName" onChange={(e) => { setPTitle(e.target.value); }}
               className="h-14 p-4 bg-gray-200 border border-gray-400 rounded-md focus:outline-none"
             />
           </div>
+
           <div className="flex flex-col my-2">
-            <label htmlFor="ProductDis" className="text-lg font-medium">
-              Product Description
-            </label>
-            <textarea
-              name=""
-              id=""
-              onChange={(e)=>{setPDescription(e.target.value)}}
+            <label htmlFor="description" className="text-lg font-medium"> Product Description </label>
+            <textarea name="description" id="description" onChange={(e) => { setPDescription(e.target.value); }}
               className="h-32 p-4 bg-gray-200 border border-gray-400 rounded-md focus:outline-none"
             ></textarea>
           </div>
+
           <div className="grid grid-cols-1 tab:grid-cols-2 lap:grid-cols-1 gap-2 items-center">
-            <div className="flex flex-col my-2">
-              <label htmlFor="">Category</label>
-              <select
-                name=""
-                id=""
-                onChange={(e)=>{setPCategory(e.target.value)}}
-                className="p-4 mt-2 border-2 border-solid rounded-md"
-              >
-                <option value="">none</option>
-                <option value="perfume">perfume</option>
-                <option value="attar">attar</option>
-              </select>
+            <div className='flex my-2'>
+            <div className="flex flex-col w-1/2 mr-2">
+              <label htmlFor="pCategory">Category</label>
+              <input type="text" name="pCategory" id="pCategory" onChange={(e) => { setPCategory(e.target.value); }}
+                className="h-14 p-4 bg-gray-200 border border-gray-400 rounded-md focus:outline-none"
+              />
+
+            </div>
+              
+            <div className="flex flex-col w-1/2">
+              <label htmlFor="subcategory">SUB CATEGORY</label>
+              <input type="text" name="subcategory" id="subcategory" onChange={(e) => { SetPSubCategory(e.target.value); }}
+                className="h-14 p-4 bg-gray-200 border border-gray-400 rounded-md focus:outline-none"
+              />
+            </div>
             </div>
 
-            <div className="flex flex-col my-2">
+            <div className='flex my-2'>
+            <div className="flex flex-col w-1/2 mr-2">
               <label htmlFor="Stock">Stock</label>
-              <input
-                type="number"
-                name="Stock"
-                id="Stock"
-                onChange={(e)=>{setPQuantity(e.target.value)}}
+              <input type="number" name="Stock" id="Stock" onChange={(e) => { setPQuantity(e.target.value); }}
                 className="bg-gray-100 p-4 mt-2 border-2 border-gray-300 focus:outline-none rounded-md"
               />
             </div>
 
-            <div className="flex flex-col my-2">
+            <div className="flex flex-col w-1/2">
               <label htmlFor="Price">Price</label>
-              <input
-                type="number"
-                name="Price"
-                id="Price"
-                onChange={(e)=>{setPPrice(e.target.value)}}
+              <input type="number" name="Price" id="Price" onChange={(e) => { setPPrice(e.target.value); }}
                 className="bg-gray-100 p-4 mt-2 border-2 border-gray-300 focus:outline-none rounded-md"
               />
             </div>
-
-            <div className="flex flex-col my-2">
-              <label htmlFor="">SUB CATEGORY</label>
-              <select
-                name=""
-                id=""
-                className="p-4 mt-2 border-2 border-solid rounded-md"
-              >
-                <option value="none">none</option>
-                <option value="Unisex">Unisex</option>
-                <option value="MAN">MAN</option>
-                <option value="WOMAN">WOMAN</option>
-              </select>
             </div>
 
-            <div className="flex flex-col my-2">
-              <label htmlFor="">Ratings</label>
-              <div className="flex gap-2 p-4 mt-2">
-                <span className="text-yellow-400 hover:scale-125"><FaRegStar/></span>
-                <span className="text-yellow-400 hover:scale-125"><FaRegStar/></span>
-                <span className="text-yellow-400 hover:scale-125"><FaRegStar/></span>
-                <span className="text-yellow-400 hover:scale-125"><FaRegStar/></span>
-                <span className="text-yellow-400 hover:scale-125"><FaRegStar/></span>
+            <div className="w-full flex gap-4 my-2 ">
+              <div className='w-4/12 flex flex-col my-2'>
+                <label htmlFor="pSize">Size</label>
+                <input type="text" name="pSize" id="pSize" onChange={(e)=>{ setPSize(e.target.value);}}
+                  className="h-14 p-4 bg-gray-200 border border-gray-400 rounded-md focus:outline-none" />
+              </div>
+
+              <div className='w-3/5 flex flex-col my-2'>
+                <label htmlFor="pTags">Tags</label>
+                <input type="text" name="pTags" id="pTags" onChange={(e)=>{ setPTags(e.target.value);}}
+                  className="h-14 p-4 bg-gray-200 border border-gray-400 rounded-md focus:outline-none" />
+              </div>
+            </div>
+
+            <div className=' w-full flex gap-4 my-2 '>
+              <div className='w-3/5 flex flex-col'>
+                <label htmlFor="dimensions">Dimensions</label>
+                <input type="text" name="dimensions" id="dimensions" onChange={(e)=>{ setPDimensions(e.target.value);}}
+                  className="h-14 p-4 bg-gray-200 border border-gray-400 rounded-md focus:outline-none" />
+              </div>
+
+              <div className='w-4/12 flex flex-col'>
+                <label htmlFor="weight">Weight</label>
+                <input type="text" name="weight" id="weight" onChange={(e)=>{ setPWeight(e.target.value);}}
+                  className="h-14 p-4 bg-gray-200 border border-gray-400 rounded-md focus:outline-none" />
               </div>
             </div>
           </div>
         </section>
+
         <section className="w-[96%] h-auto mx-auto mt-3 p-2 bg-white rounded-md">
-          <h2 className='text-2xl font-medium'>Media Files</h2>
+          <h2 className="text-2xl font-medium">Media Files</h2>
           <div className="flex gap-2 my-3">
             <div className="w-40 h-40 border-2 border-dashed border-gray-300">
-              <input type="file" name="" id="" onChange={(e)=>{setPImage(e.target.value)}} className='w-40 h-40'/>
+              <input
+                type="file"
+                name=""
+                id=""
+                onChange={(e) => {
+                  setPImages(e.target.value);
+                }}
+                className="w-40 h-40"
+              />
             </div>
 
             <div className="w-40 h-40 border-2 border-dashed border-gray-300">
-              <input type="file" name="" id="" className='w-40 h-40'/>
+              <input type="file" name="" id="" className="w-40 h-40" />
             </div>
 
             <div className="w-40 h-40 border-2 border-dashed border-gray-300">
-              <input type="file" name="" id="" className='w-40 h-40'/>
+              <input type="file" name="" id="" className="w-40 h-40" />
             </div>
 
             <div className="w-40 h-40 border-2 border-dashed border-gray-300">
-              <input type="file" name="" id="" className='w-40 h-40'/>
+              <input type="file" name="" id="" className="w-40 h-40" />
+            </div>
+
+            <div className="w-40 h-40 border-2 border-dashed border-gray-300">
+              <input type="file" name="" id="" className="w-40 h-40" />
+            </div>
+
+            <div className="w-40 h-40 border-2 border-dashed border-gray-300">
+              <input type="file" name="" id="" className="w-40 h-40" />
             </div>
           </div>
-          <button type='submit'
+          <button
+            type="submit"
             aria-label="upload the product and view"
-            className=" w-3/5 h-12 px-4 py-1 bg-blue-400 flex items-center justify-center gap-2"
+            className=" w-full h-12 px-4 py-1 bg-blue-400 flex items-center justify-center gap-2"
           >
             <FaCloudUploadAlt /> PUBLISH AND VIEW
           </button>
